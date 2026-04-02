5ff1d9c0fb52d47c7f7b90f84054ba40dbe08bce
Grangclark <enterprise.business.use@gmail.com>
2026-04-02 23:30:29 +0900

    style: center the test banner using fixed positioning

--- myBanner/script.js
+++ myBanner/script.js
@@ -5,4 +5,10 @@ testBtn.addEventListener('click', () => {
     myBanner.innerText = "拡張機能テスト中";
     document.body.appendChild(myBanner);
     myBanner.style.transform = "scale(1.2)";
+
+    // ★ 追加の4行：バナーを画面のど真ん中に固定する
+    myBanner.style.position = "fixed";
+    myBanner.style.top = "50%";
+    myBanner.style.left = "50%";
+    myBanner.style.translate = "-50% -50%";
 });
