eb2745eb20466d976a961b9ba4772051c42a6211
Grangclark <enterprise.business.use@gmail.com>
2026-04-05 20:27:35 +0900

    feat: remove YouTube endscreen recommendations using querySelector

--- my-extension/content.js
+++ my-extension/content.js
@@ -5,4 +5,10 @@ const timer = setInterval(() => {
         secondary.remove();
         clearInterval(timer); // 見つけたら監視を終了する
     }
+
+    // ★ 今日の4行：動画終了時の「おすすめ」も狙い撃ち
+    const endscreen = document.querySelector('.html5-endscreen');
+    if (endscreen) {
+        endscreen.remove();
+    }
 }, 500); // 0.5秒ごとにチェック
