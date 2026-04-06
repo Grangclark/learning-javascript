e276acd5ac0e633f39cdc63514001097f3b4297c
Grangclark <enterprise.business.use@gmail.com>
2026-04-06 09:51:12 +0900

    fix: keep monitoring and removing YouTube elements by removing clearInterval

--- my-extension/content.js
+++ my-extension/content.js
@@ -1,14 +1,8 @@
-// content.js：要素が現れるまで何度もチェックする
-const timer = setInterval(() => {
+// content.js：clearIntervalを消して、常に監視するスタイルに変更
+setInterval(() => {
     const secondary = document.getElementById('secondary');
-    if (secondary) {
-        secondary.remove();
-        clearInterval(timer); // 見つけたら監視を終了する
-    }
+    if (secondary) secondary.remove(); // 見つけるたびに消す！
 
-    // ★ 今日の4行：動画終了時の「おすすめ」も狙い撃ち
     const endscreen = document.querySelector('.html5-endscreen');
-    if (endscreen) {
-        endscreen.remove();
-    }
-}, 500); // 0.5秒ごとにチェック
+    if (endscreen) endscreen.remove(); // 出るたびに消す！
+}, 500);
