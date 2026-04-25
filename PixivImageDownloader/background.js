911a6ba8af90b2d3a8df04ab5144f6937f1f7097
Grangclark <enterprise.business.use@gmail.com>
2026-04-26 08:27:40 +0900

    debug: add logging for download URL in background service worker

--- PixivImageDownloader/background.js
+++ PixivImageDownloader/background.js
@@ -1,7 +1,22 @@
-// background.js：リファラを偽装してダウンロードを成功させる
+// background.js：URLをコンソールに出して、リファラ偽装ルールを適用する
+chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: [1],
+    addRules: [{
+        id: 1,
+        priority: 1,
+        action: {
+            type: "modifyHeaders",
+            requestHeaders: [{ header: "Referer", operation: "set", value: "https://www.pixiv.net/" }]
+        },
+        condition: { urlFilter: "pximg.net", resourceTypes: ["xmlhttprequest", "main_frame", "sub_frame", "image", "other"] }
+    }]
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === "download") {
-        // ダウンロード命令を出す
+        // ★ ここで受け取ったURLをコンソールに表示します
+        console.log("【受領】保存を開始するURL:", request.url);
+
         chrome.downloads.download({
             url: request.url,
             filename: "pixiv_image.jpg",
